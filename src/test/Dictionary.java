ddcf8068ec2a268056ca56dc7961a9f3d73a2cd8
linoy elimeleh <lino1998y@gmail.com>
2021-06-02 17:15:39 +0300

    test number 2 of patam

--- src/test/Dictionary.java
+++ src/test/Dictionary.java
@@ -1,18 +1,41 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Dictionary {
 
-	// you can add code here
-	
-	public Dictionary(String text) {
-		// you can add code here
-	}
-	
-	public List<String> getList(char c){
-		// implement in O(1)
-		return new ArrayList<>();
-	}
+    HashMap<Character, List<String>> dic;
+    // you can add code here
+
+    public Dictionary(String text) {
+        HashMap<Character, HashSet<String>> map = new HashMap<Character, HashSet<String>>();
+        Scanner s = new Scanner(text);
+        while (s.hasNext()) {
+            String oneWord = s.next();
+            char c = oneWord.charAt(0);
+            if (!map.containsKey(c)) {
+                map.put(c, new HashSet<>());
+            }
+            map.get(c).add(oneWord);
+        }
+        s.close();
+
+        dic = new HashMap<>();
+        for (char c : map.keySet()) {
+            List<String> l = new ArrayList<>();
+            l.addAll(map.get(c));
+            Collections.sort(l);
+            dic.put(c, l);
+
+        }
+        // you can add code here
+    }
+
+    public List<String> getList(char c) {
+        // implement in O(1)
+        if (dic.containsKey(c)) {
+            return dic.get(c);
+        }
+        return new ArrayList<>();
+    }
 }
--- src/test/MyStringBuilder.java
+++ src/test/MyStringBuilder.java
@@ -1,32 +1,56 @@
 package test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class MyStringBuilder {
-	// ---------- DO NOT Change ----------------
-	ArrayList<Character> buff;
-	
-	public MyStringBuilder() {
-		buff=new ArrayList<>();
-	}
-	
-	public MyStringBuilder(char...ts) {
-		buff=new ArrayList<>();
-		for(char t : ts)
-			buff.add(t);
-	}
-	
-	// -----------------------------------------
-	
-	// implement append
-	
-	// implement reverse
-	
-	// implement apply
-	
-	// implement toString
+    // ---------- DO NOT Change ----------------
+    ArrayList<Character> buff;
 
-	
+    public MyStringBuilder() {
+        buff = new ArrayList<>();
+    }
+
+    public MyStringBuilder(char... ts) {
+        buff = new ArrayList<>();
+        for (char t : ts)
+            buff.add(t);
+    }
+
+    // -----------------------------------------
+
+    public MyStringBuilder append(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            buff.add(c);
+        }
+        return this;
+    }
+
+    public String toString() {
+        char[] cs=new char[buff.size()];
+        for(int i=0;i<cs.length;i++)
+            cs[i]=buff.get(i);
+        return new String(cs);
+    }
+
+    public MyStringBuilder reverse() {
+        ArrayList<Character> arrayChars = new ArrayList<>();
+        for (int i = buff.size() - 1; i >= 0; i--) {
+            arrayChars.add(buff.get(i));
+        }
+        buff = arrayChars;
+
+        return this;
+    }
+
+    public MyStringBuilder apply(UnaryOperator<Character> uop) {
+        ArrayList<Character> t = buff;
+        buff = new ArrayList<>();
+        for (int i = 0; i < t.size(); i++)
+            buff.add(uop.apply(t.get(i)));
+        return this;
+    }
 }
